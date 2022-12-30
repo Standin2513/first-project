0c9b65b387f77a10d2c11b26085fcc0634a19aa2
Dmitry Zelenov <dz3lenov@yandex.ru>
2022-12-30 17:45:13 +0300

    Added test

---
+++ parameterizedTest.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(Parameterized.class) // Напиши аннотацию для параметризованных тестов
+public class CheckIsAdultTest {
+
+    private final int age;
+    private final boolean result;
+
+    public CheckIsAdultTest(int age, boolean result) {
+        this.age = age;
+        this.result = result; // Инициализируй поля класса в конструкторе
+    }
+
+    @Parameterized.Parameters // Пометь метод аннотацией для параметров
+    public static Object[][] getTextData() {
+        return new Object[][] {
+                {17, false},
+                {18, true},
+                {19, true},
+                {21, true},// Заполни массив тестовыми данными и ожидаемым результатом
+        };
+    }
+
+    @Test
+    public void checkIsAdultWhenAgeThenResult() {
+        Program program = new Program();
+        boolean isAdult = program.checkIsAdult(age);
+        assertEquals("Должно вернуться true, так как пользователь совершеннолетний", result, isAdult);
+    } // 123123123test123123123
+}
